5f47b50bd581689379686abfe72f6d17b947ed95
szouc <szouc@126.com>
2017-09-13 15:49:38 +0800

    Start Server Vehicle Module

--- src/client/modules/vehicle/schema.js
+++ src/client/modules/vehicle/schema.js
@@ -1,45 +1,45 @@
-import { schema, normalize, denormalize } from 'normalizr'
+// import { schema, normalize, denormalize } from 'normalizr'
 
-export const userSchema = new schema.Entity(
-  'user',
-  {},
-  { idAttribute: '_id' }
-)
+// export const userSchema = new schema.Entity(
+//   'user',
+//   {},
+//   { idAttribute: '_id' }
+// )
 
-export const driverSchema = new schema.Entity(
-  'drivers',
-  {
-    principal: userSchema,
-    secondary: userSchema
-  },
-  { idAttribute: '_id' }
-)
+// export const driverSchema = new schema.Entity(
+//   'drivers',
+//   {
+//     principal: userSchema,
+//     secondary: userSchema
+//   },
+//   { idAttribute: '_id' }
+// )
 
-export const fuelSchema = new schema.Entity(
-  'fuels',
-  {
-    applicant: userSchema
-  },
-  { idAttribute: '_id' }
-)
+// export const fuelSchema = new schema.Entity(
+//   'fuels',
+//   {
+//     applicant: userSchema
+//   },
+//   { idAttribute: '_id' }
+// )
 
-export const maintainSchema = new schema.Entity(
-  'maintenance',
-  {
-    applicant: userSchema
-  },
-  { idAttribute: '_id' }
-)
+// export const maintainSchema = new schema.Entity(
+//   'maintenance',
+//   {
+//     applicant: userSchema
+//   },
+//   { idAttribute: '_id' }
+// )
 
-export const vehicleSchema = new schema.Entity(
-  'vehicles',
-  {
-    drivers: driverSchema,
-    fuels: fuelSchema,
-    maintenance: maintainSchema
-  },
-  { idAttribute: '_id' }
-)
+// export const vehicleSchema = new schema.Entity(
+//   'vehicles',
+//   {
+//     drivers: driverSchema,
+//     fuels: fuelSchema,
+//     maintenance: maintainSchema
+//   },
+//   { idAttribute: '_id' }
+// )
 
 // const productNormalize = data => normalize(data, productSchema)
 // const productArrayNormalize = data => normalize(data, [productSchema])
